578fdcf6dbcc2ef9fde37cd1ae425aca50e51940
AngelAstorga01 <angelastorga7@hotmail.com>
2026-02-21 10:53:54 -0700

    Add program to calculate sum and average of numbers

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    const int TAM = 5;
+    int numeros[TAM];
+    int suma = 0;
+    float promedio;
+
+    cout << "Ingrese 5 numeros:" << endl;
+
+    for(int i = 0; i < TAM; i++) {
+        cout << "Numero " << i + 1 << ": ";
+        cin >> numeros[i];
+        suma += numeros[i];
+    }
+
+    promedio = suma / (float)TAM;
+
+    cout << "\nNumeros ingresados: ";
+    for(int i = 0; i < TAM; i++) {
+        cout << numeros[i] << " ";
+    }
+
+    cout << "\nSuma total: " << suma << endl;
+    cout << "Promedio: " << promedio << endl;
+
+    return 0;
+}
